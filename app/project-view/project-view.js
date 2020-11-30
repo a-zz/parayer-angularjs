004d15aa9045dae461433f82baf72311c5fc48df
a-zz <antonio@pareidolia.es>
2020-11-30 07:57:03 +0100

    Project view, added features. Still incomplete and faulty.

--- app/project-view/project-view.js
+++ app/project-view/project-view.js
@@ -9,12 +9,25 @@ angular.module('parayer.projectView', ['ngRoute'])
 		controller: 'projectViewCtrl'
 	});
 }])
-.controller('projectViewCtrl', ['$scope', '$http', function($scope, $http) {
+.controller('projectViewCtrl', ['$routeParams', '$scope', '$http', function($routeParams, $scope, $http) {
 		
 	// UI setup
+	// TODO Check tabindex-based navigation (may be faulty because of tabs)
+	// TODO Add input fields validation (see: https://docs.angularjs.org/api/ng/input/input%5Bdate%5D#examples)
 	ui.setLocation('Project');
-	$scope.tabs = [];				
+	ui.showWait(true);
+	$scope.tabs = [];		
+			
 	$scope.tabs.push(new mdc.tab.MDCTab(document.querySelector('.mdc-tab#tab-general')));
+	new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#name'));
+	new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#descr'));
+	new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#date-start'));
+	new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#date-end'));
+	new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#effort-unit'));
+	new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#effort-cap'));
+	new mdc.ripple.MDCRipple(document.querySelector('.mdc-button#submit'));
+	new mdc.ripple.MDCRipple(document.querySelector('.mdc-button#cancel'));
+	
 	$scope.tabs.push(new mdc.tab.MDCTab(document.querySelector('.mdc-tab#tab-notes')));
 	$scope.tabs.push(new mdc.tab.MDCTab(document.querySelector('.mdc-tab#tab-tasks')));
 	$scope.tabs.push(new mdc.tab.MDCTab(document.querySelector('.mdc-tab#tab-files')));
@@ -28,8 +41,55 @@ angular.module('parayer.projectView', ['ngRoute'])
 
 	// Scope initialization
 	var _usrId_ = '3602049025343d92386f90135b000f1e'; // TODO This should be global (or cookie-set?)
+	$scope.objDataUrl = `/_data/${$routeParams.projectId}`;
 	$scope.loadTabContent = function(tabId) {
-		console.log('TODO To be implemented');
+		switch(tabId) {
+		case 'tab-notes':
+			console.log('TODO To be implemented');
+			ui.showWait(false);
+			break;
+		case 'tab-tasks':
+			console.log('TODO To be implemented');
+			ui.showWait(false);
+			break;
+		case 'tab-files':
+			console.log('TODO To be implemented');
+			ui.showWait(false);
+			break;
+		case 'tab-appointments':
+			console.log('TODO To be implemented');
+			ui.showWait(false);
+			break;
+		case 'tab-history':
+			console.log('TODO To be implemented');
+			ui.showWait(false);
+			break;
+		default:
+			$http.get($scope.objDataUrl).then(function(respProject) {
+				$scope.project = {};
+				for(const key in respProject.data) {
+					if(key=='dateStart' || key=='dateEnd') {
+						if(respProject.data[key])
+							$scope.project[key] = new Date(respProject.data[key].substring(0, 4), 
+								respProject.data[key].substring(5, 7), 
+								respProject.data[key].substring(8, 10));
+					}
+					else if(key=='effortUnit' || key=='effortCap') {
+						if(respProject.data[key]) {
+							let today = new Date();
+							$scope.project[key] = new Date(today.getFullYear(), today.getMonth(), today.getDate());							
+							let effort = respProject.data[key].split(':');
+							$scope.project[key].setHours(effort[0]); 
+							$scope.project[key].setMinutes(effort[1]);
+							// TODO Strip seconds and millis
+						}
+					}
+					else
+						$scope.project[key] = respProject.data[key];
+				}
+				ui.showWait(false);
+			});
+		}
 	};
 	$scope.loadTabContent('tab-general');
 	ui.showWait(false);
@@ -37,6 +97,7 @@ angular.module('parayer.projectView', ['ngRoute'])
 	// Event handlers
 	$scope.showTab = function(tabId) {
 		
+		ui.showWait(true);
 		for(let i = 0; i<$scope.tabs.length; i++) {
 			if($scope.tabs[i].id==tabId) {
 				$scope.tabs[i].activate();
