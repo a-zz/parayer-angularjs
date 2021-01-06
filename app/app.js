db2587f2697ff4c720b7c59cc78db5d0c8afbc9a
a-zz <antonio@pareidolia.es>
2021-01-06 17:59:46 +0100

    Localized dates within acitivty grid.

--- app/app.js
+++ app/app.js
@@ -36,9 +36,6 @@ parayer.auth = {};	// -- Authentication, identification and authorization sub-na
 parayer.date = {};	// -- Date-handling utilities sub-namespace --
 (function (context) {
 	
-	let weekDays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-	let months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dec'];
-
 	// FIXME Method contract missing
 	context.computeWeek = function(selectedDate) {
 
@@ -46,7 +43,14 @@ parayer.date = {};	// -- Date-handling utilities sub-namespace --
 		for(let d = 1 - selectedDate.getDay(); d<=7-selectedDate.getDay(); d++) {
 			let date = new Date(selectedDate);
 			date.setDate(date.getDate() + d);		
-			week.push({'dt': weekDays[date.getDay()], 'dm': date.getDate(), 'mn': date.getMonth()+1, 'mt': months[date.getMonth()], 'today': (d==0)});
+			week.push({
+				"dt": date.toLocaleString(window.navigator.language, {weekday: 'short'}), 
+				"dm": date.getDate(), 
+				"mn": date.getMonth()+1, 
+				"mt":  date.toLocaleString(window.navigator.language, {month: 'short'}),
+				"dmt":  date.toLocaleString(window.navigator.language, {day: "2-digit", month: "short"}),
+				"today": (d==0)
+			});
 		}	
 		return week;
 	}
